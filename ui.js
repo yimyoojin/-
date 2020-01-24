7b50ed3fdb1ff17b114dcd304f4214227d2ab022
yimyoojin <55936622+yimyoojin@users.noreply.github.com>
2020-01-24 21:17:40 +0900

    Add files via upload

--- ui.js
+++ ui.js
@@ -41,5 +41,10 @@ $(document).ready(function(){
             }
         });
     });
+
+    $(".accordio_box ol li").click(function(){
+        $(".accordio_box ol li").removeClass("on");
+        $(this).addClass("on");
+    });
 });
 
